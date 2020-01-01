5a85beacabef389b7038a79ddfc733ba92265d24
Johannes Endres <j.endres@gmx.de>
2020-01-01 02:40:10 +0100

    web-ext configuration

---
+++ web-ext-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    artifactsDir: ".",
+    sourceDir: "src",
+    build: {
+        overwriteDest: true,
+    },
+    run:{
+        firefox: "C:\\Program Files\\Mozilla Thunderbird\\thunderbird.exe",
+        firefoxProfile: "C:\\Users\\Johannes\\AppData\\Roaming\\Thunderbird\\Profiles\\plyesg2i.debug",
+        browserConsole: true,
+    },
+};

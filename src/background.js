811a6623a743924d34866cd85b140ae631944d9d
Johannes Endres <je@johannes-endres.de>
2020-01-03 15:42:48 +0100

    'Merge' public/master and fix HTML5

--- src/background.js
+++ src/background.js
@@ -1,3 +1,26 @@
+/* 
+MIT License
+
+Copyright (c) 2020 Johannes Endres
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 var uploads = new Map();
 
 const apiBaseUrl = "/ocs/v2.php";
@@ -26,23 +49,6 @@ function encodePath(aStr) {
 
 /* If an account is removed also remove its stored data and the app token */
 browser.cloudFile.onAccountDeleted.addListener(async accountId => {
-    let accountInfo = await browser.storage.local.get(accountId);
-    if (accountInfo && accountId in accountInfo) {
-        accountData = accountInfo[accountId];
-        let authHeader = "Basic " + btoa(accountData.username + ':' + accountData.password);
-        let url = accountData.serverUrl + "/ocs/v2.php/core/apppassword?format=json";
-        let headers = {
-            "Authorization": authHeader,
-            "OCS-APIREQUEST": "true",
-        };
-        let fetchInfo = {
-            method: "DELETE",
-            headers,
-        };
-        // Just try and ignore the results
-        fetch(url, fetchInfo);
-    };
-
     browser.storage.local.remove([accountId]);
 });
 

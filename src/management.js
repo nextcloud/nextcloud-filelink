821da9647e9365314df20853c9e69bdcdd4fc440
Johannes Endres <j.endres@gmx.de>
2020-01-08 08:47:40 +0000

    Resolve "Wrap Nextcloud web service into a class"

--- src/management.js
+++ src/management.js
@@ -1,26 +1,24 @@
-/*
-MIT License
+/* MIT License
 
 Copyright (c) 2020 Johannes Endres
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
+
 let accountId = new URL(location.href).searchParams.get("accountId");
 let accountForm = document.querySelector("#accountForm");
 let serverUrl = document.querySelector("#serverUrl");
@@ -138,7 +136,8 @@ async function convertPassword() {
         // timeout, just pass on empty object
         () => { })
         .then(
-            // If the json contained a usable answer, return it, otherwise leave unchanged
+            // If the json contained a usable answer, return it, otherwise leave
+            // unchanged
             parsed => (parsed && parsed.ocs && parsed.ocs.data && parsed.ocs.data.apppassword) ?
                 parsed.ocs.data.apppassword : password.value);
 }
